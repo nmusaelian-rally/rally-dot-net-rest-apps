58510f1a83594cfe01dd06b48801006f3932436a
N M <nmusaelian@rallydev.com>
2014-04-30 08:33:43 -0600

    first commit getUserInfo

---
+++ getUserInfo.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using Rally.RestApi;
+using Rally.RestApi.Response;
+
+namespace getUserInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize the REST API
+            RallyRestApi restApi;
+            restApi = new RallyRestApi("admin@co.com", "topsecret", "https://rally1.rallydev.com", "v2.0");
+
+            //Set our Workspace and Project scopings
+            String workspaceRef = "/workspace/1111"; //use valid OID of your workspace
+            String projectRef = "/project/2222";     //use valid OID of your project
+            bool projectScopingUp = false;
+            bool projectScopingDown = true;
+
+            Request userRequest = new Request("User");
+
+            userRequest.Workspace = workspaceRef;
+            userRequest.Project = projectRef;
+            userRequest.ProjectScopeUp = projectScopingUp;
+            userRequest.ProjectScopeDown = projectScopingDown;
+
+            userRequest.Fetch = new List<string>()
+                {
+                   "Role",
+		    "CostCenter",
+                   "LastLoginDate",
+                   "OfficeLocation",
+                   "CreationDate"
+                };
+
+            userRequest.Query = new Query("UserName", Query.Operator.Equals, "nick@wsapi.com");
+            
+            QueryResult userResults = restApi.Query(userRequest);
+
+            foreach (var u in userResults.Results)
+            {
+                Console.WriteLine("----------");
+                Console.WriteLine("Role: " + u["Role"] + " CostCenter: " + u["CostCenter"] + " LastLoginDate: " + u["LastLoginDate"] + " OfficeLocation: " + u["OfficeLocation"] + " CreationDate: " + u["CreationDate"]);
+            }
+        }
+    }
+}
