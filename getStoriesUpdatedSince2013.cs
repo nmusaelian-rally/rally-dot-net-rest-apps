a3fa978ead96f2d68858d692220a6179d611fb9d
N M <nmusaelian@rallydev.com>
2014-09-12 19:08:23 -0600

    fist commmit getStoriesUpdatedSince2013

---
+++ getStoriesUpdatedSince2013.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rally.RestApi;
+using Rally.RestApi.Response;
+
+namespace aRESTgetStoriesLastUpdated
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RallyRestApi restApi;
+            restApi = new RallyRestApi("_abc123", "https://rally1.rallydev.com");
+
+            String projectRef = "/project/12352608219";
+            bool projectScopingUp = false;
+            bool projectScopingDown = false;
+
+            Request storyRequest = new Request("hierarchicalrequirement");
+            storyRequest.Project = projectRef;
+            storyRequest.ProjectScopeUp = projectScopingUp;
+            storyRequest.ProjectScopeDown = projectScopingDown;
+            storyRequest.Limit = 10000;
+            storyRequest.Query = new Query("LastUpdateDate", Query.Operator.GreaterThanOrEqualTo, "2013-01-01");
+
+            storyRequest.Fetch = new List<string>()
+                {
+                    "Name",
+                    "FormattedID"
+                };
+
+            QueryResult queryDefectResults = restApi.Query(storyRequest);
+            int count = 0;
+            foreach (var d in queryDefectResults.Results)
+            {
+                count++;
+                Console.WriteLine(count + ". FormattedID: " + d["FormattedID"] + " Name: " + d["Name"]);
+            }
+            Console.WriteLine("Found " + queryDefectResults.TotalResultCount);
+        }
+    }
+}
